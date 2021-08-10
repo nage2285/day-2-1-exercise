3e1aacd564076aaa795fb9b4ffd331ea33c8d890
cliffordcoco <88732726+nage2285@users.noreply.github.com>
2021-08-10 16:26:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+print(type(two_digit_number))
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+#print ("two_digit_number"[0] + "two_digit_number"[1])
+
+convert1 = int(first_digit)
+convert2 = int(second_digit)
+#print (first_digit)
+#print (second_digit)
+sum = convert1 + convert2
+print (sum)
+#print("new_char"[0] + "new_char"[1])
+#new_char = int(two_digit_number)
+
+
+
+
+
+
+
